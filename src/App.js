2f517c09a933736f7c509a848d17244852bf7283
bdy4119 <bdy4119@naver.com>
2023-04-13 10:20:36 +0900

    add bdy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          !백다연! <code>src/App.js</code> and save to reload.
+          bdy!백다연! <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
