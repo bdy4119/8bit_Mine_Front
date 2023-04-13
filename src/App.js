a891563c2331f5accc6aa835d205d5f34856d608
eerfsinni <snaro0123@gmail.com>
2023-04-13 14:15:34 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Link, Route, Routes, useParams } from "react-router-dom";
+import I_main from "./component/I/I_main";
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
+        <nav>
+          <Link to='/i'>I 페이지</Link>
+        </nav>
       
+        <Routes>
+          <Route path="/i" element={ <I_main /> } />
+        </Routes>
+      
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/component/I/I_add.js
@@ -0,0 +1,15 @@
+function I_add() {
+    return (
+      <div>
+        이름 :
+        생년월일 : 
+        상태 : 
+        <br/>
+        <br/>
+
+        <button>분류 추가</button>
+      </div>
+    );
+  }
+  
+  export default I_add;
